77a4509cb94a45303641bcb3168efcc2b1052aa8
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2024-09-11 19:29:05 -0400

    GUI config editing is no more

--- 2_DHCPPiScript.sh
+++ 2_DHCPPiScript.sh
@@ -4,11 +4,18 @@ sudo apt install isc-dhcp-server
 sudo sed -i 's/^INTERFACESv4=""/INTERFACESv4="eth0"/' /etc/default/isc-dhcp-server
 sudo sed -i '/^default-lease-time 600;/a \
 subnet 192.168.0.0 netmask 255.255.255.0 {\
-  range 192.168.0.5 192.168.0.250;\
-  option routers 192.168.0.1;\
-  option domain-name-servers 192.168.0.1;\
-  option domain-name "overheadcam.local";\
+ range 192.168.0.5 192.168.0.250;\
+ option routers 192.168.0.1;\
+ option domain-name-servers 192.168.0.1;\
+ option domain-name "overheadcam.local";\
 }' /etc/dhcp/dhcpd.conf
 
+# This is editing what would normally be handled by the GUI network configuration
+# We are doing it this way so we don't need to use the desktop.
+sed -i '/\[ipv4\]/{n; c\address1=192.168.0.1/24,192.168.0.1
+n; i
+}' '/etc/NetworkManager/system-connections/Wired connection 1.nmconnection'
+
+sed -i '/address1=192.168.0.1\/24,192.168.0.1/a method=manual' '/etc/NetworkManager/system-connections/Wired connection 1.nmconnection'
 
 sudo reboot now
