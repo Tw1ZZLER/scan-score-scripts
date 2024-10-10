611339fd0d8cb5dbc5fcb03a75e386494ffa5fab
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2024-10-09 20:47:02 -0400

    Clone git repo to specific directory
    Home directory is now default location for the git repository

--- 4_CodeSetupPiScript.sh
+++ 4_CodeSetupPiScript.sh
@@ -3,7 +3,7 @@
 source env/bin/activate
 sudo apt install python3-opencv
 sudo env/bin/pip install board
-git clone https://github.com/Scan-Score/led_signatures
+git clone https://github.com/Scan-Score/led_signatures ~/led_signatures
 sudo env/bin/pip install adafruit-circuitpython-lis3mdl
 
 # Quarterback RPi
